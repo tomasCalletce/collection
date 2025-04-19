730dc1da10b8bfef43d475e49e3316dc266aa148
tomas <64665732+tomasCalletce@users.noreply.github.com>
2025-04-19 12:57:54 -0500

    feat: validate webhook

--- src/server/db/schemas/users.ts
+++ src/server/db/schemas/users.ts
@@ -1,9 +1,9 @@
-import { pgTable, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
-  expires_at: timestamp("expires_at").notNull(),
+  clerk_id: text("clerk_id").notNull(),
   created_at: timestamp("created_at").defaultNow().notNull(),
   updated_at: timestamp("updated_at").defaultNow().notNull(),
 });
